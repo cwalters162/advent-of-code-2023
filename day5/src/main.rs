ee603376546bd60f1b6f09fec3ba15e0c4403325
Cherokee Walters <cherokeewalters162@gmail.com>
2023-12-05 19:48:19 -0500

    Refactored inputs for generic mapping function

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,31 +1,33 @@
-use std::num::ParseIntError;
 use std::{env, fs};
 use std::collections::HashMap;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    dbg!(&args);
-    let file_path = &args[1];
-
-    let seeds = process_seeds(&file_path);
-    let seed_to_soil = process_seed_to_soil(&file_path, seeds);
-    //let soil_to_fertilizer = process_seeds();
-    //let fertilizer_to_water = process_seeds();
-    //let water-to-light = process_seeds();
-    //let light-to-temperature = process_seeds();
-    //let temperature-to-humidity = process_seeds();
-    //let humidity-to-location = process_seeds();
+    let paths = [
+        "src/test-input/1seeds.txt",
+        "src/test-input/2seed-to-soil.txt",
+        "src/test-input/3soil-to-fertilizer.txt",
+        "src/test-input/4fertilizer-to-water.txt",
+        "src/test-input/5water-to-light.txt",
+        "src/test-input/6light-to-temperature.txt",
+        "src/test-input/7humidity-to-location.txt",
+    ];
+    let seeds = process_seeds(paths[0]);
+    let seed_to_soil = map_source_to_destination(paths[1], seeds);
+    let soil_to_fertilizer = map_source_to_destination(paths[2], seed_to_soil);
+    //let fertilizer_to_water = map_source_to_destination(paths[1], seeds);
+    //let water-to-light = map_source_to_destination(paths[1], seeds);
+    //let light-to-temperature = map_source_to_destination(paths[1], seeds);
+    //let temperature-to-humidity = map_source_to_destination(paths[1], seeds);
+    //let humidity-to-location = map_source_to_destination(paths[1], seeds);
     //process all maps
 
+    dbg!(soil_to_fertilizer);
     // let result = process(&contents);
     // println!("Lowest Location: {}", result.0);
     // println!("TBD: {}", result.1);
 }
 
-fn process_seeds(file_path_prefix: &String) -> Vec<i32> {
-    let mut file_path = file_path_prefix.clone();
-    file_path.push_str("/1seeds.txt");
-
+fn process_seeds(file_path: &str) -> Vec<i32> {
     let contents = fs::read_to_string(file_path)
         .expect("Cannot find 1seeds.txt. Please ensure it is in the directory provided");
     let split_on_whitespace = contents.split_whitespace().collect::<Vec<_>>();
@@ -42,10 +44,8 @@ fn process_seeds(file_path_prefix: &String) -> Vec<i32> {
     parse_to_numbers
 }
 
-fn process_seed_to_soil(file_path_prefix: &String, seeds: Vec<i32>) -> Vec<i32> {
-    let mut file_path = file_path_prefix.clone();
-    file_path.push_str("/2seed-to-soil.txt");
-    let contents = fs::read_to_string(file_path)
+fn map_source_to_destination(path_to_mapping_file: &str, input_for_mapping: Vec<i32>) -> Vec<i32> {
+    let contents = fs::read_to_string(path_to_mapping_file)
         .expect("Cannot find 1seeds.txt. Please ensure it is in the directory provided");
     let removed_returns = contents.replace("\r", "");
     let mut lines = removed_returns.split("\n").collect::<Vec<_>>();
@@ -75,7 +75,7 @@ fn process_seed_to_soil(file_path_prefix: &String, seeds: Vec<i32>) -> Vec<i32> 
     }
 
 
-    for source_seed in seeds.iter() {
+    for source_seed in input_for_mapping.iter() {
         let destination = seed_soil_map.get(source_seed);
         match destination {
             Some(_) => seed_to_soil_results.push(destination.unwrap().clone()),
