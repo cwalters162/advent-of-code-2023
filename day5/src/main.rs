d5d0908af14eb35cec4d12fe42141490093ef705
Cherokee Walters <cherokeewalters162@gmail.com>
2023-12-05 20:50:45 -0500

    Added consoles for very slow loop

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -3,107 +3,114 @@ use std::collections::HashMap;
 
 fn main() {
     let paths = [
-        "src/test-input/1seeds.txt",
-        "src/test-input/2seed-to-soil.txt",
-        "src/test-input/3soil-to-fertilizer.txt",
-        "src/test-input/4fertilizer-to-water.txt",
-        "src/test-input/5water-to-light.txt",
-        "src/test-input/6light-to-temperature.txt",
-        "src/test-input/7humidity-to-location.txt",
+        "src/input/1seeds.txt",
+        "src/input/2seed-to-soil.txt",
+        "src/input/3soil-to-fertilizer.txt",
+        "src/input/4fertilizer-to-water.txt",
+        "src/input/5water-to-light.txt",
+        "src/input/6light-to-temperature.txt",
+        "src/input/7temperature-to-humidity.txt",
+        "src/input/8humidity-to-location.txt",
     ];
+    println!("Processing seeds.");
     let seeds = process_seeds(paths[0]);
+    println!("Processing seds to soil.");
     let seed_to_soil = map_source_to_destination(paths[1], seeds);
+    println!("Processing soil to fertilizer.");
     let soil_to_fertilizer = map_source_to_destination(paths[2], seed_to_soil);
-    //let fertilizer_to_water = map_source_to_destination(paths[1], seeds);
-    //let water-to-light = map_source_to_destination(paths[1], seeds);
-    //let light-to-temperature = map_source_to_destination(paths[1], seeds);
-    //let temperature-to-humidity = map_source_to_destination(paths[1], seeds);
-    //let humidity-to-location = map_source_to_destination(paths[1], seeds);
-    //process all maps
+    println!("Processing fertilizer to water.");
+    let fertilizer_to_water = map_source_to_destination(paths[3], soil_to_fertilizer);
+    println!("Processing water to light.");
+    let water_to_light = map_source_to_destination(paths[4], fertilizer_to_water);
+    println!("Processing light to temperature.");
+    let light_to_temperature = map_source_to_destination(paths[5], water_to_light);
+    println!("Processing temperature to humidity.");
+    let temperature_to_humidity = map_source_to_destination(paths[6], light_to_temperature);
+    println!("Processing humidity to location.");
+    let humidity_to_location = map_source_to_destination(paths[7], temperature_to_humidity);
 
-    dbg!(soil_to_fertilizer);
-    // let result = process(&contents);
-    // println!("Lowest Location: {}", result.0);
+    println!("Lowest Location: {}", humidity_to_location.iter().min().unwrap());
     // println!("TBD: {}", result.1);
 }
 
-fn process_seeds(file_path: &str) -> Vec<i32> {
+fn process_seeds(file_path: &str) -> Vec<usize> {
     let contents = fs::read_to_string(file_path)
         .expect("Cannot find 1seeds.txt. Please ensure it is in the directory provided");
     let split_on_whitespace = contents.split_whitespace().collect::<Vec<_>>();
     let parse_to_numbers = split_on_whitespace
         .iter()
         .filter_map(|possible_number| {
-            let maybe_number = possible_number.parse::<i32>();
+            let maybe_number = possible_number.parse::<usize>();
             match maybe_number {
                 Ok(_) => Some(maybe_number.unwrap()),
                 Err(_) => None,
             }
         })
         .collect::<Vec<_>>();
+    println!("Finished processing seeds");
     parse_to_numbers
 }
 
-fn map_source_to_destination(path_to_mapping_file: &str, input_for_mapping: Vec<i32>) -> Vec<i32> {
+fn map_source_to_destination(path_to_mapping_file: &str, input_for_mapping: Vec<usize>) -> Vec<usize> {
     let contents = fs::read_to_string(path_to_mapping_file)
-        .expect("Cannot find 1seeds.txt. Please ensure it is in the directory provided");
+        .expect(&*format!("Cannot find file: {path_to_mapping_file}."));
     let removed_returns = contents.replace("\r", "");
     let mut lines = removed_returns.split("\n").collect::<Vec<_>>();
     lines.remove(0);
     let removed_whitespace = lines.iter().map(|line| line.split_whitespace().collect::<Vec<_>>()).collect::<Vec<_>>();
-    let seed_soil_mapping = removed_whitespace.iter().map(|line| {
-        let destination_start = line[0].parse::<i32>().unwrap();
-        let source_start = line[1].parse::<i32>().unwrap();
-        let range_length = line[2].parse::<i32>().unwrap();
+    println!("Making SD items.");
+    let source_destination_items = removed_whitespace.iter().map(|line| {
+        let destination_start = line[0].parse::<usize>().unwrap();
+        let source_start = line[1].parse::<usize>().unwrap();
+        let range_length = line[2].parse::<usize>().unwrap();
 
-        SeedSoilMapItem::new(destination_start,source_start, range_length)
+        SourceDestinationItem::new(destination_start, source_start, range_length)
     }).collect::<Vec<_>>();
 
-    let mut seed_to_soil_results = Vec::<i32>::new();
+    let mut source_to_destination_results = Vec::<usize>::new();
 
-    let mut seed_soil_map = HashMap::<i32, i32>::new();
+    let mut source_destination_hashmap = HashMap::<usize, usize>::new();
 
+    println!("Mapping over to build hashmaps.");
     //Why did this take me over an hour to figure out???
-    for mapping in seed_soil_mapping {
+    //How do I make this faster?!!
+    for mapping in source_destination_items.iter() {
         let d_start = mapping.destination_start;
         let s_start = mapping.source_start;
         let range_length = mapping.range_length;
 
+
+
         for i in 0..range_length {
-            seed_soil_map.insert(s_start+i, d_start+i);
+            let percent_complete = (i as f64 / range_length as f64) * 100.0;
+            println!("Percent complete: {:.1$}%", percent_complete, 2);
+
+            source_destination_hashmap.insert(s_start+i, d_start+i);
         }
     }
 
-
-    for source_seed in input_for_mapping.iter() {
-        let destination = seed_soil_map.get(source_seed);
+    println!("Processing through the source to build output values.");
+    for source in input_for_mapping.iter() {
+        let destination = source_destination_hashmap.get(source);
         match destination {
-            Some(_) => seed_to_soil_results.push(destination.unwrap().clone()),
-            None => seed_to_soil_results.push(source_seed.clone())
+            Some(_) => source_to_destination_results.push(destination.unwrap().clone()),
+            None => source_to_destination_results.push(source.clone())
         }
     }
 
-    seed_to_soil_results
+    source_to_destination_results
 }
-
-
-
-
-// fn process_seed_to_soil(file_path: &String) -> Vec<i32> {
-//     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
-//     let lines = contents.split("\n").collect::<Vec<_>>();
-// }
 
 #[derive(Debug)]
-struct SeedSoilMapItem {
-    destination_start: i32,
-    source_start: i32,
-    range_length: i32
+struct SourceDestinationItem {
+    destination_start: usize,
+    source_start: usize,
+    range_length: usize
 }
 
-impl SeedSoilMapItem {
-    fn new(destination_start: i32, source_start: i32, range_length: i32) -> SeedSoilMapItem {
-        SeedSoilMapItem {
+impl SourceDestinationItem {
+    fn new(destination_start: usize, source_start: usize, range_length: usize) -> SourceDestinationItem {
+        SourceDestinationItem {
             destination_start,
             source_start,
             range_length,
